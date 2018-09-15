05559d4d2ca75b645ba6f63035ca61b444f39643
Zaynaib Giwa <zaynaibg@gmail.com>
2018-09-15 18:44:10 -0500

    change bookshelf componenet

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -7,15 +7,13 @@ import './App.css'
 class BookShelf extends React.Component {
   render() {
     return (
-      <div class="list-books-content">
-        <div>
-        <div className="bookshelf">
-          <h2 className='bookshelf-title'>{this.props.books.bookShelf}</h2>
-          <div className='bookshelf-books'>
-            <BookList/>
-          </div>
+      <div className = "bookshelf">
+       <h2 className="bookshelf-title">Title</h2>
+        <div className="bookshelf-books">
+          <BookListbooks books={this.props.books} />
+          
         </div>
-        </div>
+
       </div>
     );
   }
